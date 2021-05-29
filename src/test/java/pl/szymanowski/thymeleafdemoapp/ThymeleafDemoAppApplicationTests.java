0fd6beea90923d82c7f47b6970e8f4634cb9ee4f
Michał Szymanowski <michal.szymanowski88@gmail.com>
2021-05-30 00:02:55 +0200

    Initial commit

---
+++ src/test/java/pl/szymanowski/thymeleafdemoapp/ThymeleafDemoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.szymanowski.thymeleafdemoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafDemoAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
